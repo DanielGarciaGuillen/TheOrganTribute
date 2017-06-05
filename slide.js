75bcf703fe1945980625c701af17d933994fa5f1
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-06-05 12:28:51 -0400

    style/change to accordion

--- slide.js
+++ slide.js
@@ -6,3 +6,21 @@ $(function(){
          .end().appendTo('.fadein');}, 
       5000);
 });
+
+
+$('.panel-collapse').on('shown.bs.collapse', function(e) {
+  var $panel = $(this).closest('.panel');
+  $('html,body').animate({
+    scrollTop: $panel.offset().top 
+  }, 1000);
+});
+
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      5000);
+});
